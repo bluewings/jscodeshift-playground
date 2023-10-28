2f06e4c9af12baa7f51ac5db1ddcb0aa4b2c8a64
sungwon-cha <mygenie3@naver.com>
2023-10-29 07:28:44 +0900

    fix

--- src/components/ScriptEditor/ScriptEditor.tsx
+++ src/components/ScriptEditor/ScriptEditor.tsx
@@ -1,12 +1,22 @@
 import Editor from '@monaco-editor/react';
+import { useAtom } from 'jotai';
+import { scriptAtom } from '../../store/script';
 import styles from './ScriptEditor.module.scss';
 
 interface IScriptEditorProps {}
 
-function ScriptEditor(props: IScriptEditorProps) {
+function ScriptEditor(_props: IScriptEditorProps) {
+  const [script, setScript] = useAtom(scriptAtom);
+
   return (
     <div className={styles.root}>
-      <Editor defaultLanguage="typescript" options={{ fontSize: 14, tabSize: 2 }} height={200} />
+      <Editor
+        defaultLanguage="typescript"
+        options={{ fontSize: 14, tabSize: 2 }}
+        height={200}
+        defaultValue={script}
+        onChange={(value) => setScript(value || '')}
+      />
     </div>
   );
 }
